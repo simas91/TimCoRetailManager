ea52afdf088b39958ad014b0b635d17bcbe00f03
simas91 <simas91@gmail.com>
2023-09-15 00:51:43 +0100

    Add API call for User Data
    added api call to get user data

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            // gets userId from the user who is loged in
+            // passing userId by method is security issue
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
