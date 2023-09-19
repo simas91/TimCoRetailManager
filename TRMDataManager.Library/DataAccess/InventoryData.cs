e57ab0f3b42d0e911b26a7741946d00d47108d4c
simas91 <simas91@gmail.com>
2023-09-19 17:57:47 +0100

    Add Admin Endpoint Data
    Adds the data for administrative endpoints. Does not yet secure the endpoints beyond authorization.

---
+++ TRMDataManager.Library/DataAccess/InventoryData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class InventoryData
+    {
+        public List<InvertoryModel> GetInventory()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<InvertoryModel, dynamic>("dbo.spInventory_GetAll", new { }, "TRMData");
+
+            return output;
+        }
+
+        public void SaveInventoryRecord(InvertoryModel item)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            sql.SaveData("dbo.spInventory_Insert", item, "TRMData");
+        }
+    }
+}
---
+++ TRMDataManager/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class InventoryController : ApiController
+    {
+        public List<InvertoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+
+            return data.GetInventory();
+        }
+
+        public void Post(InvertoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
