8f7d537c2dd65cdd6fad565fe8701f183d5db0e5
simas91 <simas91@gmail.com>
2023-09-24 17:46:08 +0100

    Added Authorization to WebApp with Login/Logout functionality.
    Added Authorization/Authentication. Login and logout pages. Verify authorization page and counter/fetch data pages for examples of different Role views.

---
+++ Portal/Authentication/AuthStateProvider.cs
@@ -0,0 +1,58 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace Portal.Authentication
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationState _anonymous;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+            _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+
+            // if not authenticated
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return _anonymous;
+            }
+
+            // if authenticated add bearer and token
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+            return new AuthenticationState(
+                    new ClaimsPrincipal(
+                        new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token),
+                            "jwtAuthType")));
+        }
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(
+                        new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token),
+                            "jwtAuthType"));
+
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void NotifyUserLogout()
+        {
+            var authState = Task.FromResult(_anonymous);
+
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}
---
+++ Portal/Authentication/AuthenticationService.cs
@@ -0,0 +1,61 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Portal.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Portal.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthenticationService(HttpClient client,
+                                     AuthenticationStateProvider authState,
+                                     ILocalStorageService localStorage)
+        {
+            _client = client;
+            _authStateProvider = authState;
+            _localStorage = localStorage;
+        }
+
+        public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", userForAuthentication.Email),
+                new KeyValuePair<string, string>("password", userForAuthentication.Password)
+            });
+
+            var authResult = await _client.PostAsync("https://localhost:7152/token", data);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                return null;
+            }
+
+            var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            await _localStorage.SetItemAsync("authToken", result.Access_Token);
+
+            ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(result.Access_Token);
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
+
+            return result;
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
+            _client.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
